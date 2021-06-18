012bf21fd9feb7b129c11b9bf834567cf69e39d9
Yurii <kasyurvik568@gmail.com>
2021-06-18 15:37:50 +0300

    лаб.2, упр. 1-6

---
+++ ITMO.WinForm.exp2/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ITMO.WinForm.exp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CloseFormBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CloseFormBTN
+            // 
+            this.CloseFormBTN.Location = new System.Drawing.Point(0, 0);
+            this.CloseFormBTN.Name = "CloseFormBTN";
+            this.CloseFormBTN.Size = new System.Drawing.Size(75, 23);
+            this.CloseFormBTN.TabIndex = 0;
+            this.CloseFormBTN.Text = "Close Form";
+            this.CloseFormBTN.UseVisualStyleBackColor = true;
+            this.CloseFormBTN.Click += new System.EventHandler(this.CloseFormBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.CloseFormBTN);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseFormBTN;
+    }
+}
+
