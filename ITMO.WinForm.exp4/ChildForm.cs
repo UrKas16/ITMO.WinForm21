0c29226ff32d03fb7972f4e4f0fb02c30ac0aa5b
Yurii <kasyurvik568@gmail.com>
2021-06-21 15:35:18 +0300

    лаб.1, упр.1-14 MDI

---
+++ ITMO.WinForm.exp4/ChildForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WinForm.exp4
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChildForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormatMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToggleMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ToggleMenuItem.Checked)
+            {
+                ToggleMenuItem.Checked = false;
+                ChildTextBox.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                ToggleMenuItem.Checked = true;
+                ChildTextBox.ForeColor = System.Drawing.Color.Blue;
+            }
+
+        }
+    }
+}
---
+++ ITMO.WinForm.exp4/ParentForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace ITMO.WinForm.exp4
+{
+    partial class ParentForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MDIMenu = new System.Windows.Forms.MenuStrip();
+            this.FileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.WindowMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.WindowCascadeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.WindowTileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MDIMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MDIMenu
+            // 
+            this.MDIMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenuItem,
+            this.WindowMenuItem});
+            this.MDIMenu.Location = new System.Drawing.Point(0, 0);
+            this.MDIMenu.MdiWindowListItem = this.WindowMenuItem;
+            this.MDIMenu.Name = "MDIMenu";
+            this.MDIMenu.Size = new System.Drawing.Size(420, 24);
+            this.MDIMenu.TabIndex = 1;
+            this.MDIMenu.Text = "MDIMenu";
+            // 
+            // FileMenuItem
+            // 
+            this.FileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewMenuItem,
+            this.ExitMenuItem});
+            this.FileMenuItem.Name = "FileMenuItem";
+            this.FileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileMenuItem.Text = "&File";
+            // 
+            // NewMenuItem
+            // 
+            this.NewMenuItem.Name = "NewMenuItem";
+            this.NewMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.NewMenuItem.Text = "&New";
+            this.NewMenuItem.Click += new System.EventHandler(this.NewMenuItem_Click);
+            // 
+            // ExitMenuItem
+            // 
+            this.ExitMenuItem.Name = "ExitMenuItem";
+            this.ExitMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ExitMenuItem.Text = "&Exit";
+            this.ExitMenuItem.Click += new System.EventHandler(this.ExitMenuItem_Click);
+            // 
+            // WindowMenuItem
+            // 
+            this.WindowMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.WindowCascadeMenuItem,
+            this.WindowTileMenuItem});
+            this.WindowMenuItem.Name = "WindowMenuItem";
+            this.WindowMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.WindowMenuItem.Text = "&Window";
+            // 
+            // WindowCascadeMenuItem
+            // 
+            this.WindowCascadeMenuItem.Name = "WindowCascadeMenuItem";
+            this.WindowCascadeMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.WindowCascadeMenuItem.Text = "&Cascade";
+            this.WindowCascadeMenuItem.Click += new System.EventHandler(this.WindowCascadeMenuItem_Click);
+            // 
+            // WindowTileMenuItem
+            // 
+            this.WindowTileMenuItem.Name = "WindowTileMenuItem";
+            this.WindowTileMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.WindowTileMenuItem.Text = "&Tile";
+            // 
+            // ParentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 320);
+            this.Controls.Add(this.MDIMenu);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.MDIMenu;
+            this.Name = "ParentForm";
+            this.Text = "Parent Form";
+            this.MDIMenu.ResumeLayout(false);
+            this.MDIMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MDIMenu;
+        private System.Windows.Forms.ToolStripMenuItem FileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NewMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem WindowMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem WindowCascadeMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem WindowTileMenuItem;
+    }
+}
+
