2b917124cb7d727d70ce29ca03d96a7f6819b397
Yurii <kasyurvik568@gmail.com>
2021-06-29 23:59:19 +0300

    лаб.3, упр.3

---
+++ ITMO.WinForm.lab3.exp3/ClickButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WinForm.lab3.exp3
+{
+    class ClickButton: System.Windows.Forms.Button
+    {
+        int mClick;
+        public int Clicks
+        {
+            get
+            {
+                return mClick;
+            }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            mClick++;
+            base.OnClick(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            System.Drawing.Graphics g = pevent.Graphics;
+            System.Drawing.SizeF stringsize;
+            stringsize = g.MeasureString(Clicks.ToString(), this.Font, this.Width);
+            g.DrawString(Clicks.ToString(), this.Font, System.Drawing.SystemBrushes.ControlText,
+              this.Width - stringsize.Width - 3, this.Height - stringsize.Height - 3);
+        }
+    }
+}
