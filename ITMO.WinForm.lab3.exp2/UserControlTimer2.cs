6d52fe08c44ed501dad4995657b7f02f0229086e
Yurii <kasyurvik568@gmail.com>
2021-06-29 23:05:22 +0300

    лаб.3, упр.2 - специализированный элемент

---
+++ ITMO.WinForm.lab3.exp2/UserControlTimer2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WinForm.lab3.exp2
+{
+    public partial class UserControlTimer2 : Control
+    {
+        public UserControlTimer2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Graphics g = pe.Graphics;
+            g.FillRectangle(Brushes.Gray, 0, 0, this.Width, this.Height);
+
+            pe.Graphics.DrawString(DateTime.Now.ToLongTimeString(), this.Font,
+                new SolidBrush(this.ForeColor), 0, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}
